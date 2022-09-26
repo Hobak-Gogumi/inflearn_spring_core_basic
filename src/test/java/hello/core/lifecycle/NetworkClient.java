16b978daf07f694e6ca0b5e2af534a72e7268820
SEOHYEON LEE <seoooohs2@naver.com>
2022-09-27 00:49:56 +0900

    빈 등록 초기화, 소멸 메서드 지정

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,16 +1,11 @@
 package hello.core.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
-
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient{
 
     private String url;
 
     public NetworkClient(){
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결메시지");
     }
 
     public void setUrl(String url){
@@ -31,14 +26,12 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("close: " + url);
     }
 
-    @Override
-    public void afterPropertiesSet() throws Exception{
+    public void init(){
         connect();
         call("초기화 연결 메시지");
     }
 
-    @Override
-    public void destroy() throws Exception{
+    public void close(){
         disconnect();
     }
 }
